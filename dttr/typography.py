8fd7c703ff569bc862b884a05aeecb1e763f64de
Ian Mancini <ianmethyst@gmail.com>
2021-08-25 03:00:04 -0300

    docs: add appearance and typography docs

--- dttr/typography.py
+++ dttr/typography.py
@@ -1,3 +1,5 @@
+"""Data module for typographies"""
+
 from functools import cache, cached_property
 from typing import Dict, Optional
 
@@ -18,17 +20,38 @@ class Typography(BasicData):
 
 
 def get_typographies_dir():
+    """Get typographies directory.
+
+    :returns: Appearances data files directory
+    """
+
     return get_data_dir() / "themes"
 
 
 def get_typography_files() -> DataFilesDict:
+    """Get typography data files.
+
+    :returns: Appearance files dictionary
+    """
+
     return get_data_files(get_typographies_dir())
 
 
 def get_typographies() -> Dict[str, Typography]:
+    """Get all typography instances.
+
+    :returns: Dict of typography instances
+    """
+
     return get_all_data_instances(get_typography_files(), get_typography_by_id)
 
 
 @cache
 def get_typography_by_id(id: str) -> Optional[Typography]:
+    """Get a specific typography instance by id.
+
+    :param id: Id of the typography to get
+    :returns: Typography instance
+    """
+
     return get_data_by_id(id, get_typography_files(), Typography)
